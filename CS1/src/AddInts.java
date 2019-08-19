9b428a8231d6228dcc28ade49a886f05ba0248ba
SergeyBordulev <skm2003@yandex.ru>
2019-08-19 16:04:27 +0700

    First Commit

---
+++ CS1/src/AddInts.java
@@ -0,0 +1,12 @@
+
+public class AddInts {
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		int sum = 0;
+		for(int i = 0; i < n; i++) {
+			int value = StdIn.readInt(); // Проверка подключения новой библиотеки
+			sum += value;
+		}
+		StdOut.println("Sum is " + sum); // Проверка подключения новой библиотеки
+	}
+}
